456eb756023e243d1c25e5c283140cda260d7f80
priyanka0901 <gupta.priyanka0901@gmail.com>
2016-09-17 11:39:30 +0530

    completed

--- js/client.js
+++ js/client.js
@@ -1,4 +1,20 @@
+$('.pure-g .js-image-colmn').each(function() {
+        $(this).parent().prepend($(this));
+    });
 
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
+$('.leftarrow').click(function() {
+	var e = $("div.insider .slideme:nth-child(3)");
+	var firstme = $("div.insider .slideme:nth-child(1)");
+	// move up:
+	firstme.insertAfter(e);
+	
+	console.log("shift left");
+});
+$('.rightarrow').click(function() {
+	var e = $("div.insider .slideme:nth-child(1)");
+	var firstme = $("div.insider .slideme:nth-child(3)");
+	// move up:
+	firstme.insertBefore(e);
+	
+	console.log("shift left");
 });
